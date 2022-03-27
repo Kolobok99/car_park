3b07f5699f0c2beca727bd12702c0b8ba973a26c
John <izolotavin99@gmail.com>
2022-03-27 15:59:34 +0500

    Добавлен вывод автомобилей

---
+++ cabinet/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+
+from .models import Car
+
+class CarMainForm(ModelForm):
+    class Meta:
+        model = Car
+        fields = (
+            'brand',
+            'registration_number',
+            'region_code',
+            'owner'
+        )
--- cabinet/views.py
+++ cabinet/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 from django.views.generic import ListView, TemplateView
 
 
-class CarsView(TemplateView):
+class CarsView(ListView):
     """Вывод всех автомобилей"""
     template_name = "index.html"
+    queryset = Car.objects.all()
+    context_object_name = "cars"
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
