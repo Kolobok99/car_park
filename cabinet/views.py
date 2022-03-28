f798e89d1322629932d331f79a24b20ed9f440ce
John <izolotavin99@gmail.com>
2022-03-28 11:31:33 +0500

    Создали Context c методами:     get_car_brands()     get_drivers()     get_regions()     get_types_of_app()
    вывели в чекбоксах варианты фильтрации

--- cabinet/views.py
+++ cabinet/views.py
@@ -12,13 +12,24 @@ class Context():
     def get_car_brands(self):
         return CarBrand.objects.all()
 
+    def get_drivers(self):
+        return Driver.objects.all()
 
+    def get_regions(self):
+        return Car.objects.all().values('region_code')
 
-class CarsView(ListView):
+    def get_types_of_app(self):
+        return TypeOfAppl.objects.all()
+
+class CarsView(Context,ListView):
     """Вывод всех автомобилей"""
     template_name = "index.html"
     queryset = Car.objects.all()
     context_object_name = "cars"
+    #
+    def setup(self, request, *args, **kwargs):
+        print(self.get_types_of_app())
+        super(CarsView, self).setup(request, *args, **kwargs)
 
 def CarFilterView(request):
     cars = CarFilter(request.GET, queryset=Car.objects.all())
