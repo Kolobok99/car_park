90fd7ec7c9e82da0c4f40c9ae65e0aa6d264c8e0
John <izolotavin99@gmail.com>
2022-04-18 12:43:41 +0500

    added app.js confirm_delete_form

---
+++ front/js/applications.js
@@ -0,0 +1,14 @@
+
+// const startDateInput = document.getElementsByName('start_date')[0];
+// // const endDateInput = document.getElementsByName('end_date')[0]
+
+// // const urgencyTypesCheckBoxs = document.getElementsByName('urgency_types');
+// // const statusTypesCheckBoxs = document.getElementsByClassName('status_types');
+// // const typesCheckBoxs = document.getElementsByClassName('types_of_apps');
+
+// document.addEventListener('click', function (event) {
+//     // if (startDateInput.value != '') {
+//     //     startDateInput.value
+//     // }
+//     console.log(startDateInput.value.lenght);
+// });
--- front/js/car.js
+++ front/js/car.js
@@ -14,19 +14,6 @@ document.addEventListener('click', function (event) {
     //кнопка закрытия формы
     const btnResetAppAdd = document.getElementsByClassName('bnt-form-app-reset-add')[0];
 
-
-    //Работа с формой редактирования
-    
-    // //кнопка открытия формы:
-    // const btnChangeApp = this.getElementsByClassName('table__btn-change-app')[0];
-    // console.log(btnChangeApp);
-    // //формы изменения заявки:
-    // const formChange = this.getElementsByClassName('form-app change')[0];
-
-    // //кнопка закрытия формы btnReset
-    // const btnResetAppChange = document.getElementsByClassName('bnt-form-app-reset-change')[0];
-
-
     //Работа с формой добавления документов:
     
     //кнопка открытия формы:
@@ -48,6 +35,33 @@ document.addEventListener('click', function (event) {
         formAddDoc.classList.toggle('data-show-or-hide-form');
         wrapper.classList.toggle('blackout');
     };
+
+
+    // Форма удаления документа:
+
+    const buttonToDeleteDoc = this.getElementsByClassName('table__btn-delete-app');
+    const formToConfirmDelete = this.getElementsByClassName('confirm-delete')[0];
+    const buttonConfirmDelete = this.getElementById('btn-confirm-delete')[0];
+    const buttonRefuseDelete = this.getElementById('btn-refuse-delete');
+    const actionInputDelete = this.getElementById('action-delete');
+
+    // При нажатии на кнопку удаления документа
+    // Получить ее id
+    // добавить этот id в value скрытого поля формы подтверждения
+    // wrapper blackout и форма подтверждения show
+
+    if (evenClassName == 'table__btn-delete-app') {
+        const buttonDeleteId = event.target.id;
+        console.log(buttonDeleteId);
+        actionInputDelete.value = buttonDeleteId;
+        // console.log(actionInputDelete.value);
+        formToConfirmDelete.classList.toggle('data-show-or-hide-form');
+        wrapper.classList.toggle('blackout');
+
+    } else if (evenClassName == buttonRefuseDelete.className) {
+        formToConfirmDelete.classList.toggle('data-show-or-hide-form');
+        wrapper.classList.toggle('blackout');
+    }
     
     // if (evenClassName == btnChangeApp.className) {
     //     const eventTargetId = event.target.id;
