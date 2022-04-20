746c6c816a755ad317aea3fa8e4f0b96a8fc710f
John <izolotavin99@gmail.com>
2022-04-20 09:50:36 +0500

    добавлен сигнал на создание авто: создание директории авто и в ней папок /avatars и /docs.

--- cabinet/signals.py
+++ cabinet/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
 
-from cabinet.models import MyUser
+from cabinet.models import MyUser, Car
 
 
 @receiver(post_save, sender=MyUser)
@@ -21,3 +21,18 @@ def post_save_myuser(created, **kwargs):
 
         #создание папки для хранения документов
         os.mkdir(f'{settings.MEDIA_ROOT}/drivers/{instance.email}/docs')
+
+@receiver(post_save, sender=Car)
+def post_save_cars(created, **kwargs):
+    instance = kwargs['instance']
+    if created:
+        print(f"{instance.registration_number} создана!")
+
+        # создание папки для машины
+        os.mkdir(f'{settings.MEDIA_ROOT}/cars/{instance.registration_number}')
+
+        # создание папки для хранения аватарок
+        os.mkdir(f'{settings.MEDIA_ROOT}/cars/{instance.registration_number}/avatars')
+
+        # создание папки для хранения документов
+        os.mkdir(f'{settings.MEDIA_ROOT}/cars/{instance.registration_number}/docs')
