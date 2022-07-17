beda43fb9ca09bb488376c7d537f8f14b4e3a520
john <izolotavin99@gmail.com>
2022-07-17 16:08:37 +0500

    Такая в принципе пригодна для деплоя. Все еще раз проверено, все работает! связка docker + docker-compose + celery + redis + telebot работает. Следующий шаг: перенос настроек

--- car_park/settings.py
+++ car_park/settings.py
@@ -20,14 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nm_h-x*1#se4anh9uf)-*art)r0e1827u_zqo8ytxn!i5c&%id'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = bool(int(os.environ.get("DEBUG", default=1)))
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
@@ -104,6 +109,20 @@ DATABASES = {
 #         'PORT': '3306',
 #     }
 # }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.e
+#         'django.db.backends.mysql',
+#         'NAME': 'db_car_park',
+#         'USER': 'root',
+#         'PASSWORD': 'rootroot',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
